e70d7e20bf4eb8ec620e7963719b84bc8097dbd5
Radif Rahman <radifrahman97@gmail.com>
2025-10-13 12:38:43 -0400

    Add comment for new apps in App.jsx

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Privacy from './components/Privacy';
 
+//new apps
 const App = () => {
   const [user, setUser] = useState(null);
 
